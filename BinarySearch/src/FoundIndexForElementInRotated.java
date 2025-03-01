1ce706a1b30dbdc9b1ebf4d47593dc2850e9bacf
sohan <sohanprasad2003@gmail.com>
2025-03-01 20:19:40 +0530

    updation rotation array logic form rotation arr

--- BinarySearch/src/FoundIndexForElementInRotated.java
+++ BinarySearch/src/FoundIndexForElementInRotated.java
@@ -1,7 +1,7 @@
 public class FoundIndexForElementInRotated {
     public static void main(String[] args) {
-        int[] arr = {2,9,10,11,18,20,30};
-       System.out.println("Target element is at : " + binarySearch(arr,0,arr.length-1,10));
+        int[] arr = {8,9,10,1,2,3};
+       System.out.println("Target element is at : " + binarySearch(arr,0,arr.length-1,3));
 
     }
 
@@ -14,14 +14,25 @@ public class FoundIndexForElementInRotated {
             {
                 return mid;
             }
-            else if(arr[mid]>target && arr[start]<=target)
+            if(arr[start]<=arr[mid])
             {
-                end = mid-1;
+                if(arr[start]<=target && arr[mid]>target)
+                {
+                    end = mid-1;
+                }
+                else {
+                    start = mid+1;
+                }
             }
-
-            else if(arr[mid]<target && target<=arr[end])
+            else
             {
-                start = mid+1;
+               if(arr[mid]<target && arr[end]>=target)
+               {
+                   start = mid+1;
+               }
+               else {
+                   end = mid-1;
+               }
             }
         }
         return -1;

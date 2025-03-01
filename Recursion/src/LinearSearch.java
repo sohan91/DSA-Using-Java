352ba48cbf308d8eca741a7cfaa7a14a456fa1f3
sohan <sohanprasad2003@gmail.com>
2025-03-01 14:59:18 +0530

    add implementation of linear search

--- Recursion/src/LinearSearch.java
+++ Recursion/src/LinearSearch.java
@@ -1,5 +1,21 @@
 public class LinearSearch {
     public static void main(String[] args) {
-        
+        int[] arr = {3,29,104,5,6};
+        int target = 3;
+        System.out.print("Index is : "+linearSearch(arr,target,0));
+
     }
+    public static int linearSearch(int[] arr,int target,int index)
+    {
+        if(index>=arr.length-1)
+        {
+            return -1;
+        }
+        if(arr[index]==target)
+        {
+            return index;
+        }
+        return linearSearch(arr,target,index+1);
+    }
+
 }

dd08f9a6a7f3ff3820ab9a978e02f1d8575c316b
sohan <sohanprasad2003@gmail.com>
2025-03-02 12:29:19 +0530

    create set class which perform operation on arry to print the sets

--- SetForArray/src/Set.java
+++ SetForArray/src/Set.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Set {
     public static void main(String[] args) {
-        
     }
+    public static List<List<Integer>> setOperation(List<Integer> list,int index)
+    {
+        if (list.isEmpty())
+        {
+            return new ArrayList<>();
+        }
+        list.add(index);
+        ArrayList<Integer> ls = setOperation(list,)
+    }
+
 }

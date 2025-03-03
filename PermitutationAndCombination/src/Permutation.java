29e33b534bf90f55f155b726fb75e3fa7b4db884
sohan <sohanprasad2003@gmail.com>
2025-03-03 23:05:18 +0530

    add comment lines

--- PermitutationAndCombination/src/Permutation.java
+++ PermitutationAndCombination/src/Permutation.java
@@ -17,6 +17,18 @@ public class Permutation {
             String first = processed.substring(0,i);
             String  second = processed.substring(i,processed.length());
             permutation(first+ch+second,unProcessed.substring(1));
+
+//            For permutation("cba", ""): Prints "cba"
+//
+//            For permutation("bca", ""): Prints "bca"
+//
+//            For permutation("bac", ""): Prints "bac"
+//
+//            For permutation("cab", ""): Prints "cab"
+//
+//            For permutation("acb", ""): Prints "acb"
+//
+//            For permutation("abc", ""): Prints "abc"
         }
     }
 

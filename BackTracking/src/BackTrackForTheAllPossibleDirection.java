40bec4773b7b6232336aebce4f9cab1f8e50e885
sohan <sohanprasad2003@gmail.com>
2025-03-05 21:51:16 +0530

    found all possible solution form all direction

--- BackTracking/src/BackTrackForTheAllPossibleDirection.java
+++ BackTracking/src/BackTrackForTheAllPossibleDirection.java
@@ -1,5 +1,34 @@
 public class BackTrackForTheAllPossibleDirection {
     public static void main(String[] args) {
+        int end = 3;
+        boolean[][] booleans = {
+                {true,true,true},
+                {true,true,true},
+                {true,true,true}};
+             allDirectionPath(booleans,"",0,0,end);
+    }
+    public static void allDirectionPath(boolean[][] arr,String name,int i,int j,int end) {
+        if (i == end - 1 && j == end - 1) {
+            System.out.println(name);
+            return;
+        }
 
+        if (!(arr[i][j])) {
+            return;
+        }
+        arr[i][j] = false;
+        if (i < end-1) {
+            allDirectionPath(arr, name + "D", i+1, j, end);
+        }
+        if (j < end-1) {
+            allDirectionPath(arr, name + "R", i, j+1, end);
+        }
+        if (i >0) {
+            allDirectionPath(arr, name + "U", i - 1, j, end);
+        }
+        if (j>0) {
+            allDirectionPath(arr, name + "L", i , j-1, end);
+        }
+        arr[i][j] = true;
     }
 }

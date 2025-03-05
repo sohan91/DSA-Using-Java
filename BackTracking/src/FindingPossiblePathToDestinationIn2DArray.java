db2fac3699a56dbb1d47c46354cf94296eef53ea
sohan <sohanprasad2003@gmail.com>
2025-03-05 20:46:09 +0530

    create and working on obstacle in 2D array

--- BackTracking/src/FindingPossiblePathToDestinationIn2DArray.java
+++ BackTracking/src/FindingPossiblePathToDestinationIn2DArray.java
@@ -3,6 +3,15 @@ public class FindingPossiblePathToDestinationIn2DArray {
         int[][] arr = new int[3][3];
         System.out.println("Possible Path count is : "+countForBestPath(arr,1,1,arr.length));
         path("",arr,1,1,arr.length);
+
+        boolean[][] booleans = {
+                {true,true,true},
+                {true, false, true},
+                {true,true,true},
+                {true,true,true},
+                {true,true,true}};
+        System.out.println(booleans.length);
+        obstacle(booleans,"",0,0,booleans.length);
     }
     public static int countForBestPath(int[][] arr,int i,int j,int end)
     {
@@ -34,4 +43,31 @@ public class FindingPossiblePathToDestinationIn2DArray {
             path(name + "R", arr, i, j + 1, end);
         }
     }
+
+    public static void obstacle(boolean[][] arr, String name, int i, int j, int end)
+    {
+        if(arr.length == arr[0].length)
+        {
+            if(i == arr.length-1 && j == arr[i].length-1)
+            {
+                System.out.println(name);
+                return;
+            }
+            if(!(arr[i][j]))
+            {
+                return;
+            }
+            if(i<arr.length-1) {
+                obstacle(arr, name + "D", i + 1, j, end);
+            }
+            if(j<arr[i].length-1) {
+                obstacle(arr, name + "R", i, j + 1, end);
+            }
+
+        }else {
+            System.out.println("Cannot Perform.....");
+        }
+
+    }
+
 }

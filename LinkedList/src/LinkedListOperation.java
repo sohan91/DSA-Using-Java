221850a1ff8631a8da6e81c4d2bcd2d35d993f7f
sohan <sohanprasad2003@gmail.com>
2025-03-14 19:41:07 +0530

    perform insertionAtMiddle operation on LinkedList

--- LinkedList/src/LinkedListOperation.java
+++ LinkedList/src/LinkedListOperation.java
@@ -7,20 +7,30 @@ class Node
        this.data = data;
        this.next = null;
    }
+    Node(int data,Node next)
+    {
+        this.data = data;
+        this.next = next;
+    }
 }
 class Operation{
+    private  static int size = 1;
     Node tail = null;
     Node head = null;
 
     public void insertAtFirst(int data)
     {
         Node node = new Node(data);
+        if(head == null)
+        {
+            tail = node;
+            head = node;
+            size++;
+            return;
+        }
         node.next = head;
         head = node;
-        if(tail == null)
-        {
-            tail = head;
-        }
+        size++;
     }
 
     public void insertAtLast(int data)
@@ -29,9 +39,37 @@ class Operation{
         if(tail == null) {
             head = node;
             tail = node;
+            size++;
+            return;
         }
         tail.next = node;
         tail = node;
+        size++;
+    }
+
+    public void insertInMiddle(int data,int index)
+    {
+        if(index>size-1 || index<0)
+        {
+            System.out.println("Invalid Index...Can't Perform Operation !!!");
+            return;
+        }
+        if(index == 0)
+        {
+            insertAtFirst(data);
+            return;
+        }
+        if(index == size-1)
+        {
+            insertAtLast(data);
+            return;
+        }
+        Node temp = head;
+        for(int i = 1;i<index;i++)
+        {
+            temp = temp.next;
+        }
+        temp.next = new Node(data,temp.next);
     }
     public void display()
     {
@@ -58,8 +96,10 @@ public class LinkedListOperation {
        operation.insertAtLast(20);
        operation.insertAtLast(12);
        operation.insertAtLast(30);
-        System.out.println("-".repeat(30));
-        System.out.print("Insertion at from Last/Tail : ");
+       operation.insertInMiddle(44,3);
+       operation.display();
+        System.out.println("\n"+"-".repeat(30));
+        System.out.print("\nInsertion at from Last/Tail : ");
         operation.display();
         System.out.println("\n"+"-".repeat(30));
         operation.insertAtFirst(39);

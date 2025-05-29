208f8e16cd726d17b01543ccc0d16a079466bb5e
sohan <sohanprasad2003@gmail.com>
2025-05-29 19:06:21 +0530

    working on AVL insertion

--- BinaryTrees/AVLInsertion.java
+++ BinaryTrees/AVLInsertion.java
@@ -1,24 +1,25 @@
-public class AVLInsertion {
-    public static void main(String[] args) {
+class AVLInsertion
+{
+    public static void main(String[] args)
+    {
         AVLOperation operation = new AVLOperation();
-
-        for(int i = 1;i<25;i++)
+        for(int i = 0;i<49;i++)
         {
             operation.insert(i);
         }
         operation.display();
     }
 }
-
 class TNode
 {
     int data;
+    int height;
     TNode left;
     TNode right;
-    int height;
     TNode(int data)
     {
-        this.data = data;
+        this.data =data;
+        this.height = 1;
     }
 }
 class AVLOperation
@@ -26,95 +27,120 @@ class AVLOperation
     TNode root;
     public void insert(int data)
     {
-       root = insert(root,data);
+        root = insert(root,data);
     }
-    public TNode insert(TNode node,int data)
+    public TNode insert(TNode tNode, int data)
     {
-        if(node == null)
+        if(tNode == null)
         {
             return new TNode(data);
         }
-        if(node.data>data)
+        if(tNode.data > data)
         {
-            node.left = insert(node.left,data);
-        } else if (node.data<data) {
-            node.right = insert(node.right,data);
+            tNode.left = insert(tNode.left,data);
         }
-        else {
-            return node;
-        }
-        node.height = 1+Math.max(getHeight(node.left),getHeight(node.right));
-        int balance = getBalance(node);
-        if(balance > 1 && data<node.left.data)
+        else if(tNode.data<data)
         {
-            return rightRotation(node);
+            tNode.right = insert(tNode.right,data);
         }
-        if(balance < -1 && data > node.right.data)
+        else
         {
-            return leftRotation(node);
+            return tNode;
         }
-        if(balance < -1 && data<node.right.data)
+
+        tNode.height = 1+Math.max(getHeight(tNode.left),getHeight(tNode.right));
+        int balance = getBalance(tNode);
+
+        //RR imbalance -- apply LL rotation
+        if(balance < -1 && data > tNode.right.data)
         {
-            node.right = rightRotation(node.right);
-            return leftRotation(node);
+            return leftRotate(tNode);
         }
-        if(balance > 1 && data > node.left.data)
+
+        //LL imbalance -- apply RR rotation
+        if(balance > 1 && data < tNode.left.data)
         {
-            node.left = leftRotation(node.left);
-            return rightRotation(node);
+            return rightRotate(tNode);
         }
-        return node;
+        //LR imbalance -- apply LR rotation
+        if(balance > 1 && data > tNode.left.data)
+        {
+            tNode.left = leftRotate(tNode.left);
+            return rightRotate(tNode);
+        }
+        //Rl imbalance -- apply RL rotation
+        if(balance < -1 && data < tNode.right.data)
+        {
+            tNode.right = rightRotate(tNode.right);
+            return leftRotate(tNode);
+        }
+        return tNode;
     }
-    private int getHeight(TNode node)
+    //perform LL rotation -- for RR imbalance
+    public TNode leftRotate(TNode tNode)
     {
-        return node == null ? -1 :node.height;
-    }
-    private int getBalance(TNode node)
-    {
-        return node == null ? 0:getHeight(node.left)-getHeight(node.right);
-    }
-    private TNode leftRotation(TNode node)
-    {
-        TNode newRoot = node.right;
+        TNode newRoot = tNode.right;
         TNode temp = newRoot.left;
-        newRoot.left = node;
-        node.right = temp;
-        node.height = 1+Math.max(getHeight(node.left),getHeight(node.right));
-        newRoot.height = 1+Math.max(getHeight(newRoot.left),getHeight(newRoot.right
-        ));
+
+        newRoot.left = tNode;
+        tNode.right = temp;
+
+        tNode.height= 1+Math.max(getHeight(tNode.left),getHeight(tNode.right));
+        newRoot.height = 1+Math.max(getHeight(tNode.left),getHeight(tNode.right));
         return newRoot;
     }
-    private TNode rightRotation(TNode node)
+
+    //perform RR rotation -- for LL imbalance
+    public TNode rightRotate(TNode tNode)
     {
-        TNode newRoot = node.left;
+        TNode newRoot = tNode.left;
         TNode temp = newRoot.right;
-        newRoot.right = node;
-        node.left = temp;
-        node.height = 1+Math.max(getHeight(node.left),getHeight(node.right));
-        newRoot.height = 1+Math.max(getHeight(newRoot.left),getHeight(newRoot.right));
-                return newRoot;
+
+        newRoot.right = tNode;
+        tNode.left = temp;
+        tNode.height= 1+Math.max(getHeight(tNode.left),getHeight(tNode.right));
+        newRoot.height = 1+Math.max(getHeight(tNode.left),getHeight(tNode.right));
+        return newRoot;
+    }
+    public int getHeight(TNode tNode)
+    {
+        if(tNode == null)
+        {
+            return 0;
+        }
+        return tNode.height;
+    }
+    public int getBalance(TNode tNode)
+    {
+        if(tNode == null)
+        {
+            return 0;
+        }
+        return (getHeight(tNode.left)-getHeight(tNode.right));
     }
     public void display()
     {
         display(root,0);
     }
-    private void display(TNode node,int lev)
+    public void display(TNode tNode, int lvl)
     {
-        if(node == null)
+        if(tNode == null)
         {
             return;
         }
-        display(node.right,lev+1);
-        if(lev != 0)
+        display(tNode.right,lvl+1);
+        if(lvl != 0)
         {
-            for (int i = 0; i < lev; i++) {
+            for(int i = 0;i<lvl;i++)
+            {
                 System.out.print("|\t\t");
             }
-            System.out.println("|--->"+node.data);
+            System.out.println("|--->"+ tNode.data);
         }
-        else {
-            System.out.println("|--->"+node.data);
+        else
+        {
+            System.out.println("|--->"+ tNode.data);
         }
-        display(node.left,lev+1);
+        display(tNode.left,lvl+1);
     }
 }
